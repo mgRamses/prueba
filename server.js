6f1233ec61413da5ae7c3afabcf9692b1df9f6af
Ram <mg.ramses@gmail.com>
2019-09-17 11:58:11 -0500

    b

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/prueba1768'));
+app.use(express.static(__dirname + '/dist/prueba'));
 
 app.get('/*', function (req, res) {
 
-    res.sendFile(path.join(__dirname + '/dist/prueba1768/index.html'));
+    res.sendFile(path.join(__dirname + '/dist/prueba/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
